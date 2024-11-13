722739ed315b0fa368612383ab1833f0243ec94f
Kamel R <kamel.r@wayne.edu>
2024-11-13 18:36:16 -0500

    added the code to check the winner

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -13,6 +13,37 @@ void TicTacToe::setLocation(int row, int column, char player)
 	board[row-1][column-1] = player;
 }
 
+bool TicTacToe::checkWin(char player)
+{
+	// check rows 
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (board[i][0] == board[i][1] == board[i][2] == player)
+			return true;
+
+	}
+
+	//check columns 
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (board[0][i] == board[1][i] == board[2][i] == player)
+			return true;
+
+	}
+
+	// check diagonals 
+	if (board[0][0] == board[1][1] == board[2][2] == player)
+		return true;
+
+	if (board[0][2] == board[1][1] == board[2][0] == player)
+		return true;
+
+
+	return false;
+}
+
 void TicTacToe::printBoard()
 {
 	cout << "-------------\n";
