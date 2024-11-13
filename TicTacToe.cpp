3ccfa66f6809cdb56c894e2b2551a261491ca820
Kamel R <kamel.r@wayne.edu>
2024-11-13 18:08:31 -0500

    added the set location function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,10 +2,15 @@
 
 void TicTacToe::setLocation(int row, int column, char player)
 {
-	// validation 
+	// validation
+	if (row < 1 || row >3 || column < 1 || column>3)
+		throw 1;// wrong input for coords
+	if (player != 'o' && player != 'O' && player != 'x' && player != 'X')
+		throw 2;// wrong player symbol 
+
 
 	// set the location to the player char 
-
+	board[row-1][column-1] = player;
 }
 
 void TicTacToe::resetGame()
